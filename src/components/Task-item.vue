ddff6f9fafa2971205e83bbb70f7418b6b81fced
ArshadRahim <arshadrahim000@gmail.com>
2021-04-01 18:02:40 +0500

    vuejs-todo-app

---
+++ src/components/Task-item.vue
@@ -0,0 +1,19 @@
+ <template>
+    
+ <li> 
+            <button> <i class="far fa-circle"></i> {{task.title}}</button>
+            <button @click="$emit('remove')" ><i class="far fa-trash-alt"></i></button>
+          </li>
+    
+</template>
+
+<script>
+    export default {
+        name : "taskItem",
+        props : ['task']
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
